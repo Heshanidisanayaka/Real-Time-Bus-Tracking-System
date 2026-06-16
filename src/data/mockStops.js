fd4ab85ff1e75816ad11c19f1ec1ea7b21715b53
Heshanidisanayaka <heshanidisanayaka11@gmail.com>
2026-06-16 11:03:23 +0530

    feat: Add mock stops data for Nearby Bus Stops

---
+++ src/data/mockStops.js
@@ -0,0 +1,27 @@
+export const mockStops = [
+  {
+    id: 1,
+    name: 'Central Station',
+    coordinates: [40.713, -74.006]
+  },
+  {
+    id: 2,
+    name: 'Financial District',
+    coordinates: [40.715, -74.001]
+  },
+  {
+    id: 3,
+    name: 'South Ferry',
+    coordinates: [40.704, -74.010]
+  },
+  {
+    id: 4,
+    name: 'Union Square',
+    coordinates: [40.735, -73.991]
+  },
+  {
+    id: 5,
+    name: 'Madison Square Garden',
+    coordinates: [40.750, -73.993]
+  }
+];
